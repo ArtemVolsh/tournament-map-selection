e6b5c7ba0d84f78af9c3c5cf86acffe36a600e5b
Artem Voloshyn <artemvolsh@gmail.com>
2025-08-16 15:36:57 +0200

    feat: Add bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div class="container py-4 px-3 mx-auto">
       <h1>Hello, Bootstrap and Vite!</h1>
+      <div class="d-flex justify-content-center mb-4">
+        <Card />
+      </div>
       <button class="btn btn-primary">Primary button</button>
     </div>
   );
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,5 @@
+<div class="card">
+  <h3>Map Placeholder</h3>
+</div>;
+
+export default Card;
